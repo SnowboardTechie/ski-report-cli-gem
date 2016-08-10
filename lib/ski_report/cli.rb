ac44e2b6b1ca53bc81c3a30d0341554e19516acb
SnowboardTechie <bryan.thompson518@gmail.com>
2016-08-09 23:07:31 -0600

    Added basic running layout using list_states and report

--- lib/ski_report/cli.rb
+++ lib/ski_report/cli.rb
@@ -2,15 +2,34 @@ class SkiReport::CLI
 
   def call
     puts "Today's Ski Reports"
-    list_states
+    report
   end
 
   def list_states
-    puts "Please select a state to view resorts"
+    
     puts <<-DOC
     1. Colorado
     2. Utah
     DOC
+    puts "Please select a state to view resorts"
   end
+
+  def report
+    list_states
+
+    input = nil
+
+    while input != "exit"
+      list_states
+      input = gets.chomp.downcase
+
+      if input.to_i == 1
+        puts "You selected CO"
+      elsif input.to_i == 2
+        puts "You selected UT"
+      end
+    end
+  end
+
 
 end
