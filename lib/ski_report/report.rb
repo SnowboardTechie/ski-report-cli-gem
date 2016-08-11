22e4ad7c7b6fe8c6412dd1936b4ffd7035394f88
SnowboardTechie <bryan.thompson518@gmail.com>
2016-08-10 18:55:54 -0600

    Setup basic scrape in scrape_ots, still needs completion

--- lib/ski_report/report.rb
+++ lib/ski_report/report.rb
@@ -1,13 +1,33 @@
 class SkiReport::Report
 
-  def self.scrape_ots(state)
-    #returns either array or hash for print_report
+  attr_accessor :state
+
+  def self.scrape_ots(state_url)
+    #returns array of objects that have a name, 24 hour snow total, 72 hours snow total
+    #base depth
+    #[{:name => 'Arapahoe Basin', :24hr => 0, :72hr => 0, :base => 30}, {:name => 'Arapahoe Basin', :24hr => 0, :72hr => 0, :base => 30}]
+    resorts = []
+    doc = Nokogiri::HTML(open("http://www.onthesnow.com/" + state_url + "/skireport.html?&ud=1&o=resort"))
+    doc.css(".resScrollCol8 table tr").each do |resort|
+      if resort.css('.name').text != ""
+        # binding.pry
+        new_resort = {
+          :name => resort.css('.name').text
+        }
+        resorts << new_resort
+      end
+    end
+    resorts
+
   end
 
   def self.print_report(state)
+    @state = state
     report_data = scrape_ots(state.gsub(/\s/, '-').downcase) #takes state and formats it for scraping
+
     puts "#{state}" #using as test to verify state is appropriately setup here
     #uses report_data from get_report to print
+    #Snow Report's for Colorado:
   end
 
 end
