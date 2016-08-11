8672bba91c835b2cfbab579aa2a38fe82a3d55e7
SnowboardTechie <bryan.thompson518@gmail.com>
2016-08-11 12:37:38 -0600

    Adjusted requires to require_relative

--- lib/ski_report.rb
+++ lib/ski_report.rb
@@ -1,9 +1,9 @@
 require "bundler/setup"
-require "ski_report"
-require "ski_report/version"
 require "nokogiri"
 require "open-uri"
 require "pry"
+require_relative "./ski_report"
+require_relative "./ski_report/version"
 require_relative "./ski_report/report"
 require_relative "./ski_report/cli"
 
