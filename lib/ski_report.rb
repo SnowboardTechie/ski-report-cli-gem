6a71c38b8f7ecde2a48e9ebfffa8ac66066e892e
SnowboardTechie <bryan.thompson518@gmail.com>
2016-08-10 18:55:29 -0600

    added pry

--- lib/ski_report.rb
+++ lib/ski_report.rb
@@ -3,6 +3,7 @@ require "ski_report"
 require "ski_report/version"
 require "nokogiri"
 require "open-uri"
+require "pry"
 require_relative "./ski_report/report"
 require_relative "./ski_report/cli"
 
