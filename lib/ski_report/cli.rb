7506f641a007af01a0a08691826bac193deb0bac
SnowboardTechie <bryan.thompson518@gmail.com>
2016-08-11 20:23:11 -0600

    changed list_states to fit a standard console width

--- lib/ski_report/cli.rb
+++ lib/ski_report/cli.rb
@@ -11,7 +11,7 @@ class SkiReport::CLI
           ]
   
   def call
-    puts "                 ************* Welcome to Ski Reports *************"
+    puts "********************* Welcome to Ski Reports *********************"
     menu
   end
 
@@ -35,15 +35,15 @@ class SkiReport::CLI
   end
 
   def list_states
-    puts <<-HEREDOC
-      1. #{@@SKI_STATES[0]}       8. #{@@SKI_STATES[7]}       15. #{@@SKI_STATES[14]}      22. #{@@SKI_STATES[21]} 29. #{@@SKI_STATES[28]}
-      2. #{@@SKI_STATES[1]}      9. #{@@SKI_STATES[8]}          16. #{@@SKI_STATES[15]}       23. #{@@SKI_STATES[22]}           30. #{@@SKI_STATES[29]}
-      3. #{@@SKI_STATES[2]}  10. #{@@SKI_STATES[9]}         17. #{@@SKI_STATES[16]}        24. #{@@SKI_STATES[23]}         31. #{@@SKI_STATES[30]}
-      4. #{@@SKI_STATES[3]}    11. #{@@SKI_STATES[10]}      18. #{@@SKI_STATES[17]} 25. #{@@SKI_STATES[24]}   32. #{@@SKI_STATES[31]}
-      5. #{@@SKI_STATES[4]} 12. #{@@SKI_STATES[11]} 19. #{@@SKI_STATES[18]}    26. #{@@SKI_STATES[25]}   33. #{@@SKI_STATES[32]}
-      6. #{@@SKI_STATES[5]}       13. #{@@SKI_STATES[12]}      20. #{@@SKI_STATES[19]}    27. #{@@SKI_STATES[26]}      34. #{@@SKI_STATES[33]}
-      7. #{@@SKI_STATES[6]}    14. #{@@SKI_STATES[13]}     21. #{@@SKI_STATES[20]}      28. #{@@SKI_STATES[27]}
-    HEREDOC
+    puts "1. #{@@SKI_STATES[0]}      9. #{@@SKI_STATES[8]}           17. #{@@SKI_STATES[16]}         25. #{@@SKI_STATES[24]}"
+    puts "2. #{@@SKI_STATES[1]}     10. #{@@SKI_STATES[9]}         18. #{@@SKI_STATES[17]}  26. #{@@SKI_STATES[25]}"
+    puts "3. #{@@SKI_STATES[2]}  11. #{@@SKI_STATES[10]}      19. #{@@SKI_STATES[18]}     27. #{@@SKI_STATES[26]}"
+    puts "4. #{@@SKI_STATES[3]}    12. #{@@SKI_STATES[11]} 20. #{@@SKI_STATES[19]}     28. #{@@SKI_STATES[27]}"
+    puts "5. #{@@SKI_STATES[4]} 13. #{@@SKI_STATES[12]}      21. #{@@SKI_STATES[20]}       29. #{@@SKI_STATES[28]}"
+    puts "6. #{@@SKI_STATES[5]}       14. #{@@SKI_STATES[13]}     22. #{@@SKI_STATES[21]} 30. #{@@SKI_STATES[29]}"
+    puts "7. #{@@SKI_STATES[6]}    15. #{@@SKI_STATES[14]}      23. #{@@SKI_STATES[22]}           31. #{@@SKI_STATES[30]}"
+    puts "8. #{@@SKI_STATES[7]}     16. #{@@SKI_STATES[15]}       24. #{@@SKI_STATES[23]}         32. #{@@SKI_STATES[31]}"
+    puts "               33. #{@@SKI_STATES[32]}     34. #{@@SKI_STATES[33]}"
 
     puts "\nTo view resorts please select a state by entering its number or type 'exit'"
   end
